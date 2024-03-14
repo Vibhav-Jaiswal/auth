dd7f007ecd8199bb9178985929b5420f1b22fc7b
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-03-14 12:09:16 +0530

    update header component and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,20 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className=" bg-slate-200">
       <div className="flex justify-between items-center max-w-5xl mx-auto p-3">
@@ -11,7 +13,17 @@ const Header = () => {
         <ul className="flex gap-4">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <Link to="/sign-in">Sign in</Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
+            ) : (
+              <li>Sign in</li>
+            )}
+          </Link>
         </ul>
       </div>
     </div>
